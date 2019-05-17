8b45a6ec6521ba6d48c8f5d834ce64977834ede4
Teo G Nistor <teo.g.nistor@gmail.com>
2019-05-18 00:33:53 +0100

    Double slider works on lengths but is slow

--- src/frontend/components/SampleFeatureOverrideProvider.tsx
+++ src/frontend/components/SampleFeatureOverrideProvider.tsx
@@ -3,10 +3,12 @@ import { ColorDef, ElementProps } from "@bentley/imodeljs-common";
 
 export class SampleFeatureOverrideProvider implements FeatureOverrideProvider {
 
-  private _elements: ElementProps[];
+  private readonly _elements: ElementProps[];
+  private readonly _depthSlice: number[];
 
-  public constructor(elements: ElementProps[]) {
+  public constructor(elements: ElementProps[], depthSlice: number[]) {
     this._elements = elements;
+    this._depthSlice = depthSlice;
   }
 
   // interface function to set feature overrides
@@ -15,14 +17,17 @@ export class SampleFeatureOverrideProvider implements FeatureOverrideProvider {
     const defaultAppearance = FeatureSymbology.Appearance.fromRgba(ColorDef.white);
     const lightGreen = FeatureSymbology.Appearance.fromRgba(ColorDef.from(0, 255, 0)); // green
     const darkGreen = FeatureSymbology.Appearance.fromRgba(ColorDef.from(0, 100, 0)); // green
-    // const invisible = FeatureSymbology.Appearance.fromRgba(ColorDef.from(0, 0, 0, 255));
+    const invisible = FeatureSymbology.Appearance.fromRgba(ColorDef.from(0, 0, 0, 255));
 
     // set default appearance for all elements
     _overrides.setDefaultOverrides(defaultAppearance);
     // set appearance of elements passed in
     if (this._elements) this._elements.forEach( (element: ElementProps) => {
       if (element.id){
-        if (element.geometry_Length < 50) {
+        if (element.geometry_Length < this._depthSlice[0] || element.geometry_Length > this._depthSlice[1]) {
+          _overrides.overrideElement(element.id, invisible);
+        }
+        else if (element.geometry_Length < 50) {
           _overrides.overrideElement(element.id, darkGreen);
         } else {
           _overrides.overrideElement(element.id, lightGreen);
